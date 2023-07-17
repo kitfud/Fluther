48fde44b7491ce0f50dcbf88ff151e8c59d58669
Kit Fuderich <kitfuderich@gmail.com>
2023-07-16 22:28:29 -0700

    material UI and basic form added

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,24 +1,65 @@
 import logo from './jellies_01.jpg';
 import './App.css';
 import { ConnectWallet } from "@thirdweb-dev/react";
+import { useAddress } from "@thirdweb-dev/react";
+
+import { ThemeProvider,createTheme } from '@mui/material';
+import {  Box, TextField, Typography, Card, Button} from "@mui/material"
+
 
 function App() {
+
+  const address = useAddress();
+  const theme = createTheme({
+    palette:{
+      mode:"dark"
+    }
+  })
+
   return (
 
-    <div className="App">
-        <ConnectWallet
+
+<>
+{console.log(address)}
+<ThemeProvider theme={theme}>
+  <Box>
+  <ConnectWallet
               dropdownPosition={{
                 side: "bottom",
                 align: "center",
               }}/>
-               
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Front end placeholder.
-        </p>
-      </header>
-    </div>
+  </Box>
+
+
+      
+  <Card variant="outlined" sx={{ display: 'inline-block', backgroundColor: "gray" }}>
+  <Typography color="primary" component="h1" sx={{ fontSize: 20, fontWeight: 600, padding: 2}}>Create Dollar Cost Average:</Typography>
+
+  
+<Box >
+<TextField id="filled-basic" label="Amount" variant="filled" ></TextField>
+</Box> 
+
+<Box >
+<TextField id="filled-basic" label="Token1" variant="filled" ></TextField>
+</Box> 
+
+<Box >
+<TextField id="filled-basic" label="Token2" variant="filled" ></TextField>
+</Box> 
+
+<Box >
+<TextField id="filled-basic" label="Time Interval (sec)" variant="filled" ></TextField>
+</Box> 
+
+<Button variant="contained" color="warning">Submit Agreement</Button>
+          
+  </Card> 
+</ThemeProvider>
+     
+
+</>
+      
   );
 }
 
