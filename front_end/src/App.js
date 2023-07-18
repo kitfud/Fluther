7ab0b231304117a3daae02252ca07fb3ff3a3a58
Kit Fuderich <kitfuderich@gmail.com>
2023-07-17 17:01:39 -0700

    stateful form added and centered all elements on page for consistent device width responsiveness

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,13 +1,19 @@
-import logo from './jellies_01.jpg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
 import { ConnectWallet } from "@thirdweb-dev/react";
 import { useAddress } from "@thirdweb-dev/react";
-
 import { ThemeProvider,createTheme } from '@mui/material';
-import {  Box, TextField, Typography, Card, Button} from "@mui/material"
+import {Grid,Box, TextField, Typography, Card, Button} from "@mui/material"
 
 
 function App() {
+  const [amount, setAmount] = useState("")
+  const [token1,setToken1] = useState("")
+  const [token2,setToken2] = useState("")
+  const [interval, setInterval] = useState("")
+
+  const [contractParamsSet, setContractParams] = useState(false)
+  const [userAddress,setUserAddress] = useState(null)
 
   const address = useAddress();
   const theme = createTheme({
@@ -16,48 +22,68 @@ function App() {
     }
   })
 
+useEffect(()=>{
+if(amount!== "" && token1!== "" & token2!=="" & interval !=="")
+{
+  setContractParams(true)
+}
+else{
+  setContractParams(false)
+}
+},[amount,token1,token2,interval])
+
+const submitAgreement =()=>{
+let data = {"user":address,"amount":amount,"token1":token1,"token2":token2,"interval":interval}
+console.log(JSON.stringify(data))
+}
+
   return (
-
-
 <>
-{console.log(address)}
+{/* {address?console.log(address):null} */}
+
 <ThemeProvider theme={theme}>
-  <Box>
+  <Box sx={{marginBottom:'10px', marginTop:'10px'}} display="flex" alignItems="center" justifyContent="center">
   <ConnectWallet
               dropdownPosition={{
-                side: "bottom",
+                side: "top",
                 align: "center",
               }}/>
   </Box>
-
-
-      
-  <Card variant="outlined" sx={{ display: 'inline-block', backgroundColor: "gray" }}>
+  
+  <Grid   container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justify="center"
+  style={{ minHeight: '100vh' }}>
+  <Card variant="outlined" sx={{ alignSelf:'center',display: 'inline-block', backgroundColor: "gray" }}>
   <Typography color="primary" component="h1" sx={{ fontSize: 20, fontWeight: 600, padding: 2}}>Create Dollar Cost Average:</Typography>
 
   
 <Box display="flex" alignItems="center" justifyContent="center">
-<TextField id="filled-basic" label="Amount" variant="filled" ></TextField>
+<TextField onChange={(e)=> setAmount(e.target.value)} id="filled-basic" label="Amount" variant="filled" ></TextField>
 </Box> 
 
 <Box display="flex" alignItems="center" justifyContent="center">
-<TextField id="filled-basic" label="Token1" variant="filled" ></TextField>
+<TextField onChange={(e)=> setToken1(e.target.value)} id="filled-basic" label="Token1" variant="filled" ></TextField>
 </Box> 
 
 <Box display="flex" alignItems="center" justifyContent="center">
-<TextField id="filled-basic" label="Token2" variant="filled" ></TextField>
+<TextField onChange={(e)=> setToken2(e.target.value)} id="filled-basic" label="Token2" variant="filled" ></TextField>
 </Box> 
 
 <Box display="flex" alignItems="center" justifyContent="center" >
-<TextField id="filled-basic" label="Time Interval (sec)" variant="filled" ></TextField>
+<TextField onChange={(e)=> setInterval(e.target.value)} id="filled-basic" label="Time Interval (sec)" variant="filled" ></TextField>
 </Box> 
 
-<Box sx={{marginTop:'20px'}} display="flex" alignItems="center" justifyContent="center">
-<Button variant="contained" color="warning">Submit Agreement</Button>
-</Box>
-
-          
+{ contractParamsSet?
+<Box sx={{marginTop:'20px',marginBottom:"20px"}} display="flex" alignItems="center" justifyContent="center">
+<Button onClick={submitAgreement} variant="contained" color="warning">Submit Agreement</Button>
+</Box>  : <Box sx={{marginTop:'20px',marginBottom:"20px"}}></Box>
+}    
   </Card> 
+  </Grid>
+  
 </ThemeProvider>
 </>
       
