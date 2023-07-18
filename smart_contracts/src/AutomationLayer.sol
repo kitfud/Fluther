7ac9db5867bafc4c64928ec219d1986d092bbdc4
Eduardo W. da Cunha <eduardowestc@gmail.com>
2023-07-18 14:08:35 -0300

    reestructured AutomatioLayer smart contract and added new Interface (IAutomatedContract)

--- smart_contracts/src/AutomationLayer.sol
+++ smart_contracts/src/AutomationLayer.sol
@@ -16,6 +16,7 @@ import {IAutomationLayer} from "./interfaces/IAutomationLayer.sol";
 import {INodeSequencer} from "./interfaces/INodeSequencer.sol";
 import {IUniswapV2Router02} from "./interfaces/IUniswapV2Router02.sol";
 import {IDollarCostAveraging} from "./interfaces/IDollarCostAveraging.sol";
+import {IAutomatedContract} from "./interfaces/IAutomatedContract.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
@@ -37,30 +38,37 @@ contract AutomationLayer is
 
     /* solhint-disable */
     // base types
-    uint256 private nextAccountNumber;
-    address private duh;
-    uint256 private minimumDuh;
-    address private sequencerAddress;
-    uint256 private automationFee = 100;
+    uint256 private s_nextAccountNumber;
+    address private s_duh;
+    uint256 private s_minimumDuh;
+    address private s_sequencerAddress;
+    uint256 private s_automationFee;
+    address private s_oracleAddress;
 
     // mappings and arrays
-    mapping(uint256 accountNumber => Accounts) private accounts;
-    mapping(address => bool) private isNodeRegistered;
-
-    // constants
-    address private constant WMATIC =
-        0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+    mapping(uint256 accountNumber => Account) private s_accounts;
+    mapping(address => bool) private s_isNodeRegistered;
 
     /* solhint-enable */
 
     /// -----------------------------------------------------------------------
-    /// Modifiers
+    /// Modifiers (or functions as modifiers)
     /// -----------------------------------------------------------------------
+
+    function __validateAccountNumber(Status status) private pure {
+        if (status != Status.SET) {
+            revert AutomationLayer__InvalidAccountNumber();
+        }
+    }
+
+    function __onlyOracle() private view {
+        require(msg.sender == s_oracleAddress);
+    }
 
     // check to see if nodes have enough tokens to be valid nodes
     modifier hasSufficientTokens() {
         require(
-            INodeSequencer(sequencerAddress).hasSufficientTokens() == true,
+            INodeSequencer(s_sequencerAddress).hasSufficientTokens() == true,
             "Insufficient token balance."
         );
         _;
@@ -68,7 +76,7 @@ contract AutomationLayer is
     //Checks with sequencer to ensure nodes are taking turns
     modifier isCurrentNode() {
         require(
-            tx.origin == INodeSequencer(sequencerAddress).getCurrentNode(),
+            tx.origin == INodeSequencer(s_sequencerAddress).getCurrentNode(),
             "Not current node"
         );
         _;
@@ -78,11 +86,18 @@ contract AutomationLayer is
     /// Constructor
     /// -----------------------------------------------------------------------
 
-    constructor() {
-        minimumDuh = 0;
-
-        duh = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174; //USDC on Polygon
-        //duh = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; //WETH on Ethereum
+    constructor(
+        address duh,
+        uint256 minimumDuh,
+        address sequencerAddress,
+        uint256 automationFee,
+        address oracleAddress
+    ) {
+        s_duh = duh;
+        s_minimumDuh = minimumDuh;
+        s_sequencerAddress = sequencerAddress;
+        s_automationFee = automationFee;
+        s_oracleAddress = oracleAddress;
     }
 
     /// -----------------------------------------------------------------------
@@ -90,45 +105,68 @@ contract AutomationLayer is
     /// -----------------------------------------------------------------------
 
     //contracts call create account to register a new account to be automated
-    function createAccount(uint256 id) external returns (uint256) {
-        uint256 accountNumber = nextAccountNumber;
+    function createAccount(
+        uint256 id,
+        address user,
+        address contractAddress
+    ) external returns (uint256) {
+        uint256 accountNumber = s_nextAccountNumber;
 
-        accounts[accountNumber] = Accounts(msg.sender, id, false);
+        s_accounts[accountNumber] = Account(
+            user,
+            contractAddress,
+            id,
+            Status.SET
+        );
 
-        ++nextAccountNumber;
+        unchecked {
+            ++s_nextAccountNumber;
+        }
 
-        emit AccountCreated(msg.sender);
+        emit AccountCreated(user, contractAddress, id);
+
         return accountNumber;
     }
 
     //Contracts call this function to cancel an account
     function cancelAccount(uint256 accountNumber) external {
-        require(accountNumber < nextAccountNumber, "Invalid payment index");
+        Account storage account = s_accounts[accountNumber];
 
-        Accounts storage account = accounts[accountNumber];
+        __validateAccountNumber(account.status);
 
-        require(
-            msg.sender == account.account,
-            "Only account creator can cancel account"
+        if (
+            msg.sender != account.user &&
+            msg.sender != account.automatedContract
+        ) {
+            revert AutomationLayer__NotAllowed();
+        }
+
+        account.status = Status.CANCELLED;
+
+        emit AccountCancelled(
+            accountNumber,
+            account.user,
+            account.automatedContract
         );
-
-        account.cancelled = true;
-
-        emit AccountCancelled(accountNumber, account.account);
     }
 
     //If transaction is ready to be triggered, nodes call trigger function
-    function trigger(
+    function simpleAutomation(
         uint256 accountNumber
     ) external hasSufficientTokens isCurrentNode {
-        require(accountNumber < nextAccountNumber, "Invalid account index");
+        Account memory account = s_accounts[accountNumber];
 
-        Accounts memory account = accounts[accountNumber];
-        require(!account.cancelled, "The profile has been canceled.");
+        __validateAccountNumber(account.status);
 
-        emit TransactionSuccess(accountNumber);
+        emit TransactionSuccess(
+            accountNumber,
+            account.user,
+            account.automatedContract
+        );
 
-        IDollarCostAveraging(account.account).trigger(account.id);
+        IAutomatedContract(account.automatedContract).simpleAutomation(
+            account.id
+        );
         // Transfer the tokens
         /*  require(
             IERC20(duh).transferFrom(account.account, tx.origin, automationFee),
@@ -137,8 +175,23 @@ contract AutomationLayer is
         */
     }
 
+    // FOR SECURITY REASONS
+    function withdraw(uint256 amount) external onlyOwner {
+        //transferFrom(address(this), owner, amount)
+    }
+
     function setSequencerAddress(address _sequencerAddress) external onlyOwner {
-        sequencerAddress = _sequencerAddress;
+        s_sequencerAddress = _sequencerAddress;
+    }
+
+    function setOracleAddress(address _oracleAddress) external onlyOwner {
+        s_oracleAddress = _oracleAddress;
+    }
+
+    function setAutomationFee(uint256 _automationFee) external {
+        __onlyOracle();
+
+        s_automationFee = _automationFee;
     }
 
     /// -----------------------------------------------------------------------
@@ -146,28 +199,20 @@ contract AutomationLayer is
     /// -----------------------------------------------------------------------
 
     // check to se if tranaction is ready to be triggered
-    function checkTrigger(uint256 accountNumber) external view returns (bool) {
-        require(accountNumber < nextAccountNumber, "Invalid account index");
-
-        Accounts memory account = accounts[accountNumber];
-        return IDollarCostAveraging(account.account).checkTrigger(account.id);
-    }
-
-    // //look up indices using contract address
-    // function getIndicesFromAddress(
-    //     address accountAddress
-    // ) external view returns (uint256[] memory) {
-    //     return addressToIndices[accountAddress];
-    // }
-
-    //check if account has been cancelled before calling trigger
-    function isAccountCanceled(
+    function checkSimpleAutomation(
         uint256 accountNumber
     ) external view returns (bool) {
-        require(accountNumber < nextAccountNumber, "Invalid account index");
+        Account memory account = s_accounts[accountNumber];
 
-        Accounts storage account = accounts[accountNumber];
+        __validateAccountNumber(account.status);
 
-        return account.cancelled;
+        return
+            IAutomatedContract(account.user).checkSimpleAutomation(account.id);
+    }
+
+    function getAccount(
+        uint256 accountNumber
+    ) external view returns (Account memory) {
+        return s_accounts[accountNumber];
     }
 }
