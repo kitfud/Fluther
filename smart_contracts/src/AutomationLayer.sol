5aae4a7ff45eb2f0c939ddbc585557cd25e767b3
Levi <levi.webb@gmail.com>
2023-07-19 12:33:05 -0700

    updated checkSimpleAutomation function

--- smart_contracts/src/AutomationLayer.sol
+++ smart_contracts/src/AutomationLayer.sol
@@ -342,7 +342,7 @@ contract AutomationLayer is
         __validateAccountNumber(account.status);
 
         return
-            IAutomatedContract(account.user).checkSimpleAutomation(account.id);
+            IAutomatedContract(account.automatedContract).checkSimpleAutomation(account.id);
     }
 
     /// @inheritdoc IAutomationLayer
