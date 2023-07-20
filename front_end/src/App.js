abb2518a5b42f7cca568d82a50a3537795453521
GZRdev <gloriareilly@protonmail.com>
2023-07-19 19:59:52 -0700

    Made code easier to read. Added comments. Organized code for consistency and readability.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import { ConnectWallet } from "@thirdweb-dev/react";
 import { useAddress } from "@thirdweb-dev/react";
 import { ThemeProvider,createTheme } from '@mui/material';
-
-
 import {Grid,Box, TextField, Typography, Card, Button} from "@mui/material"
 
 const theme = createTheme({
@@ -29,8 +27,7 @@ const theme = createTheme({
       },
       inputProps:{
         
-          style:{color:"red"}
-        
+          style:{color:"red"}  
       }
     },
     MuiCard: {
@@ -44,9 +41,10 @@ const theme = createTheme({
           zIndex: 0,
           raised:true
         },
-  },
-
-}}})
+      },
+    }
+  }
+})
 
 function App() {
   const [amount, setAmount] = useState("")
@@ -60,72 +58,103 @@ function App() {
   const address = useAddress();
   
 
-useEffect(()=>{
-if(amount!== "" && token1!== "" & token2!=="" & interval !=="")
-{
-  setContractParams(true)
-}
-else{
-  setContractParams(false)
-}
-},[amount,token1,token2,interval])
+  useEffect(()=>{
+    if(amount!== "" && token1!== "" & token2!=="" & interval !=="") {
+      setContractParams(true)
+    }
+    else {
+      setContractParams(false)
+    }
+  },[amount,token1,token2,interval])
 
-const submitAgreement =()=>{
-let data = {"user":address,"amount":amount,"token1":token1,"token2":token2,"interval":interval}
-console.log(JSON.stringify(data))
-}
+  const submitAgreement =()=> {
+    let data = {
+      "user":address,
+      "amount":amount,
+      "token1":token1,
+      "token2":token2,
+      "interval":interval
+    }
+    console.log(JSON.stringify(data))
+  }
 
   return (
-<>
-{/* {address?console.log(address):null} */}
+    <>
+      {/* {address?console.log(address):null} */}
 
-<ThemeProvider theme={theme}>
-  <Box sx={{marginBottom:'10px', marginTop:'4vh'}} display="flex" alignItems="center" justifyContent="center">
-  <ConnectWallet
-              dropdownPosition={{
-                side: "top",
-                align: "center",
-              }}/>
-  </Box>
-  
-  <Grid   container
-  spacing={0}
-  direction="column"
-  alignItems="center"
-  justify="center"
-  style={{ minHeight: '100vh' }}>
-  <Card variant="outlined" sx={{ alignSelf:'center',display: 'inline-block', backgroundColor:theme.palette.secondary.main }}>
-  <Typography color="#1f25e2" component="h1" sx={{ fontSize: 20, fontWeight: 700, padding: 2}}>Create Dollar Cost Average:</Typography>
+      <ThemeProvider theme={theme}>
+        
+        {/* CONNECT WALLET BUTTON */}
+        <Box sx={{marginBottom:'10px', marginTop:'4vh'}} display="flex" alignItems="center" justifyContent="center">
+          <ConnectWallet
+            dropdownPosition={{
+              side: "top",
+              align: "center",
+            }}/>
+        </Box>
+        
+        <Grid   container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '100vh' }}>
 
-  
-<Box display="flex" alignItems="center" justifyContent="center">
-<TextField     
-onChange={(e)=> setAmount(e.target.value)} id="filled-basic" label="Amount" variant="filled" ></TextField>
-</Box> 
+          {/* MAIN CARD */}
+          <Card variant="outlined" sx={{ 
+            alignSelf:'center',
+            display: 'inline-block',
+            backgroundColor:theme.palette.secondary.main
+            }}>
+            <Typography color="#1f25e2" component="h1" sx={{
+               fontSize: 20,
+               fontWeight: 700,
+               padding: 2
+              }}>
+                Create Dollar Cost Average:
+            </Typography>
 
-<Box display="flex" alignItems="center" justifyContent="center">
-<TextField onChange={(e)=> setToken1(e.target.value)} id="filled-basic" label="Token1" variant="filled" ></TextField>
-</Box> 
+            {/* AMOUNT BOX FIELD */}  
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <TextField onChange={(e)=> setAmount(e.target.value)} id="filled-basic" label="Amount" variant="filled" >
+              </TextField>
+            </Box>
 
-<Box display="flex" alignItems="center" justifyContent="center">
-<TextField onChange={(e)=> setToken2(e.target.value)} id="filled-basic" label="Token2" variant="filled" ></TextField>
-</Box> 
+            {/* TOKEN 1 BOX FIELD */}
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <TextField onChange={(e)=> setToken1(e.target.value)} id="filled-basic" label="Token1" variant="filled" >
+              </TextField>
+            </Box>
 
-<Box display="flex" alignItems="center" justifyContent="center" >
-<TextField onChange={(e)=> setInterval(e.target.value)} id="filled-basic" label="Time Interval (sec)" variant="filled" ></TextField>
-</Box> 
+            {/* TOKEN 2 BOX FIELD */}
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <TextField onChange={(e)=> setToken2(e.target.value)} id="filled-basic" label="Token2" variant="filled" >
+              </TextField>
+            </Box> 
 
-{ contractParamsSet?
-<Box sx={{marginTop:'20px',marginBottom:"20px"}} display="flex" alignItems="center" justifyContent="center">
-<Button onClick={submitAgreement} variant="contained" color="warning">Submit Agreement</Button>
-</Box>  : <Box sx={{marginTop:'20px',marginBottom:"20px"}}></Box>
-}    
-  </Card> 
-  </Grid>
-  
-</ThemeProvider>
-</>
+            {/* TIME INTERVAL BOX FIELD */}
+            <Box display="flex" alignItems="center" justifyContent="center" >
+              <TextField onChange={(e)=> setInterval(e.target.value)} id="filled-basic" label="Time Interval (sec)" variant="filled" >
+              </TextField>
+            </Box> 
+
+            {/* "SUBMIT AGREEMENT" BUTTON LOGIC */}
+            { contractParamsSet
+              ?
+              <Box sx={{marginTop:'20px',marginBottom:"20px"}} display="flex" alignItems="center" justifyContent="center">
+                <Button onClick={submitAgreement} variant="contained" color="warning">
+                  Submit Agreement
+                </Button>
+              </Box>
+              :
+              <Box sx={{marginTop:'20px',marginBottom:"20px"}}>
+              </Box>
+            }    
+          </Card> 
+        </Grid>
       
+      </ThemeProvider>
+    </>
   );
 }
 
