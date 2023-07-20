3d0b144dccb7fe5585e50c3e3f04d2ecc604f3a8
Kit Fuderich <kitfuderich@gmail.com>
2023-07-19 22:30:10 -0700

    fixed drop down positioning and adjusted time frames

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -153,10 +153,11 @@ function App() {
             </Box> 
 
             {/* TIME INTERVAL BOX FIELD */}
-            <Box display="flex" alignItems="center" justifyContent="center" >
-              <FormControl fullWidth>
+            <Box display="flex" alignItems="center" justifyContent="center"  >
+           
+              <FormControl sx={{width:'70%', marginTop:'40px'}}>
                 <InputLabel id="demo-simple-select-label">
-                  Hours
+                  Set Investment Interval
                 </InputLabel>
                 <Select
                   id="filled-basic"
@@ -165,33 +166,13 @@ function App() {
                   onChange={ (e) => setInterval(e.target.value) }
                 >
                   {/* WILL FIGURE OUT A BETTER WAY TO DO THIS PART LATER */}
-                  <MenuItem value={1}>1</MenuItem>
-                  <MenuItem value={2}>2</MenuItem>
-                  <MenuItem value={3}>3</MenuItem>
-                  <MenuItem value={4}>4</MenuItem>
-                  <MenuItem value={5}>5</MenuItem>
-                  <MenuItem value={6}>6</MenuItem>
-                  <MenuItem value={7}>7</MenuItem>
-                  <MenuItem value={8}>8</MenuItem>
-                  <MenuItem value={9}>9</MenuItem>
-                  <MenuItem value={10}>10</MenuItem>
-                  <MenuItem value={11}>11</MenuItem>
-                  <MenuItem value={12}>12</MenuItem>
-                  <MenuItem value={13}>13</MenuItem>
-                  <MenuItem value={14}>14</MenuItem>
-                  <MenuItem value={15}>15</MenuItem>
-                  <MenuItem value={16}>16</MenuItem>
-                  <MenuItem value={17}>17</MenuItem>
-                  <MenuItem value={18}>18</MenuItem>
-                  <MenuItem value={19}>19</MenuItem>
-                  <MenuItem value={20}>20</MenuItem>
-                  <MenuItem value={21}>21</MenuItem>
-                  <MenuItem value={22}>22</MenuItem>
-                  <MenuItem value={23}>23</MenuItem>
-                  <MenuItem value={24}>24</MenuItem>
+                  <MenuItem value={300}>5 Minute</MenuItem>
+                  <MenuItem value={3600}>Hourly</MenuItem>
+                  <MenuItem value={86400}>Daily</MenuItem>
+                  <MenuItem value={604800}>Weekly</MenuItem>
                 </Select>
-
               </FormControl>
+        
               {/* 
               <TextField onChange={(e)=> setInterval(e.target.value)} id="filled-basic" label="Time Interval (sec)" variant="filled" >
               </TextField>
