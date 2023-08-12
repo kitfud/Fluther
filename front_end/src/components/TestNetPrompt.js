782e2297d208515b7800817ad0680bd0d0b136d1
Kit Fuderich <kitfuderich@gmail.com>
2023-08-12 12:57:20 -0700

    incomplete button

--- front_end/src/components/TestNetPrompt.js
+++ front_end/src/components/TestNetPrompt.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useSDK,switchNetwork} from "@thirdweb-dev/react";
+import { Button,Box } from '@mui/material';
 
 const TestNetPrompt = () => {
+
+const sdk = useSDK();
+
+const switchNetwork = (event)=>{
+    sdk.SwitchNetwork(11155111);
+}
   return (<>
   <div>This is a TestNet APP- Connect TO Sepolia Network. </div>
   <div>Edit this prompt is in components file</div>
+
+  <Box><Button variant="contained" 
+  onClick={switchNetwork}
+  color="success">CLICK TO CHANGE NETWORK</Button></Box>
   </>
     
     
