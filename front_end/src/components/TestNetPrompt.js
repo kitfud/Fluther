80ae85210ab6dd11570ced44c6ad447f8602ee2e
Kit Fuderich <kitfuderich@gmail.com>
2023-08-12 10:19:16 -0700

    adapted app for user network changes, added components to prompt user to change network and for hero landing page additions, removed token fountain if user address is not logged

---
+++ front_end/src/components/TestNetPrompt.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TestNetPrompt = () => {
+  return (<>
+  <div>This is a TestNet APP- Connect TO Sepolia Network. </div>
+  <div>Edit this prompt is in components file</div>
+  </>
+    
+    
+  )
+}
+
+export default TestNetPrompt
